e054a6d496aaad9421efbd02da9cd43e88d7b815
Raphlore <raphlore@yahoo.com>
2024-06-25 13:06:08 +0100

    complete signup functionality

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 dotenv.config()
@@ -12,9 +13,15 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log(err)
 })
 
+
+
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({
+  origin: 'http://localhost:5173' // Replace with your frontend URL
+}));
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
